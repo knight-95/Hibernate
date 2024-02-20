4b0178c77fe27da73382ad3c3fa85f924186ac87
Yash Gupta <yashgupta13.in@gmail.com>
2024-02-20 11:47:26 +0530

    uploading database using hibernate
    Using annotations, uploading data, images to the database

--- src/main/java/com/hibernate/projectHibernate/App.java
+++ src/main/java/com/hibernate/projectHibernate/App.java
@@ -1,5 +1,8 @@
 package com.hibernate.projectHibernate;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Date;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -9,22 +12,41 @@ import org.hibernate.cfg.Configuration;
  *
  */
 public class App {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.out.println("Project Started!");
 		SessionFactory factory = new Configuration().configure().buildSessionFactory();
-		
-		//creating student
+
+		// creating student
 		Student st = new Student();
 		st.setId(102);
-		st.setName("Kirti");
+		st.setName("Yash Gupta");
 		st.setCity("Indore");
 		System.out.println(st);
-		
-		Session session= factory.openSession();
-		
-		//starting a transaction
+
+		// creating object of Student class
+		Address ad = new Address();
+		ad.setStreet("Indra Nagar");
+		ad.setCity("Gwalior");
+		ad.setOpen(true);
+		ad.setAddedDate(new Date());
+		ad.setTemperature(13.2);
+		System.out.println(ad);
+
+		// Reading Image
+		FileInputStream fis = new FileInputStream("src/main/java/car.jpg");
+		byte[] data = new byte[fis.available()];
+		fis.read(data);
+		ad.setImage(data);
+
+		Session session = factory.openSession();
+
+		// starting a transaction
 		session.beginTransaction();
 		session.save(st);
+
+		// save is deprecated from Hibernate 6.0
+//		session.save(ad);
+		session.persist(ad);
 		session.getTransaction().commit();
 		session.close();
 	}
